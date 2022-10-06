14d1ff1ada90e52ab97de179674f1c181a280d68
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-10-06 17:02:28 +0200

    vezba default parameters

---
+++ vezba.js
@@ -0,0 +1,15 @@
+const karte = [];
+
+const izborKarte = function (tribina, sektor, broj, cena = 600 * broj) {
+  const karta = {
+    tribina,
+    sektor,
+    broj,
+    cena,
+  };
+  console.log(karta);
+  karte.push(karta);
+};
+
+izborKarte('zapad', 2, 3);
+izborKarte('sever', undefined, 3, 300);
