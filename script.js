4350b4ba577e10237366237d731dbecf47480bf7
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-10-06 17:02:10 +0200

    default parameters

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 200 * numPassengers
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+createBooking('LH123', undefined, 3); //undefined ako zelimo da preskocimo unos nekog od podataka (parametara)
