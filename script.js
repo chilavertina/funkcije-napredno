1d53b194c5bcf1be5d5ede92fe47e4649213cf38
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-10-08 20:01:49 +0200

    ponasanje menjanja vrednosti u primitivnim i reference tipovima

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 const bookings = [];
 
 const createBooking = function (
@@ -21,3 +21,29 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 3); //undefined ako zelimo da preskocimo unos nekog od podataka (parametara)
+*/
+
+const flight = 'LH234';
+const passenger = {
+  name: 'Uros Martic',
+  passport: 592834,
+};
+
+const checkIn = function (flightNum, passengerData) {
+  flightNum = 'LH999';
+  passengerData.name = 'Mr. ' + passengerData.name;
+
+  if (passengerData.passport === 592834) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, passenger);
+console.log(flight);
+console.log(passenger);
+
+//isto kao ovo...
+const flightNum = flight;
+const passengerData = passenger;
