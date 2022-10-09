1b5d9118f90f09250677c1c78a8de4fcbc173ecc
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-10-09 13:11:33 +0200

    vezba callback funkcija

--- vezba.js
+++ vezba.js
@@ -15,3 +15,27 @@ const izborKarte = function (tribina, sektor, broj, cena = 600 * broj) {
 izborKarte('zapad', 2, 3);
 izborKarte('sever', undefined, 3, 300);
 */
+
+const spojenaRecenica = function (recenica) {
+  return recenica.replaceAll('-', ' ').toLowerCase().replaceAll('0', 'o');
+};
+
+spojenaRecenica('pr0lEtEri-svih-zEmalja-ujedinitE-sE');
+console.log(spojenaRecenica('pr0lEtEri-svih-zEmalja-ujedinitE-sE'));
+
+const ispravnaRecenica = function (recenica) {
+  const [first, ...others] = recenica
+    .replaceAll('-', ' ')
+    .toLowerCase()
+    .split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+ispravnaRecenica('pr0lEtEri-svih-zEmalja-ujedinitE-sE');
+console.log(ispravnaRecenica('pr0lEtEri-svih-zEmalja-ujedinitE-sE'));
+
+const resenje = function (string, fn) {
+  console.log(fn(string));
+};
+
+resenje('pr0lEtEri-svih-zEmalja-ujedinitE-sE', ispravnaRecenica);
